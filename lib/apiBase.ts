47d3e1ea2adf3e3e73ea5795f22061cb4c035d10
RitikPadhy <ritipkp2002@gmail.com>
2025-09-14 10:19:24 +0530

    Initial setup for the project is done

--- lib/apiBase.ts
+++ lib/apiBase.ts
@@ -1,6 +1,6 @@
 export function getApiBase() {
   if (process.env.NODE_ENV === "production") {
-    return ""; // relative path in production
+    return "http://13.233.212.132:80"; // or your domain like https://api.mindtrack.com
   } else {
     return "http://localhost:8000"; // local backend
   }
